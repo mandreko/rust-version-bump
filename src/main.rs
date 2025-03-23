ab8234227b91fe508370d4a49b78c1cf37a44e2a
Matt Andreko <mandreko@gmail.com>
2025-03-23 15:06:51 -0400

    Got it all working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::env;
 use std::fs;
+use std::fs::File;
 use std::path::{Path};
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 use regex::Regex;
-use semver::Version;
+use serde_yaml;
 use xmltree::{Element, XMLNode};
+use plist::Value as PlistValue;
+use serde_json::Value as JsonValue;
 
 fn main() -> io::Result<()> {
     let input_version = env::var("INPUT_VERSION")
@@ -14,25 +17,22 @@ fn main() -> io::Result<()> {
 
     let path = Path::new(&file_path);
 
-    let file_name = path.file_name().unwrap();
-    let file_extension = path
-        .extension()
-        //.and_then(OsStr::to_str);
-        .unwrap()
-        .to_str()
-        .unwrap();
+    let file_name = path.file_name()
+        .ok_or("Failed to extract file name from path")?;
 
+    let file_extension = path.extension()
+        .and_then(|ext| ext.to_str())
+        .ok_or("Failed to extract file extension as string")?;
 
     match file_extension {
-        ".xml" | ".props" | ".csproj" => update_xml(&input_version, path),
-        ".json" => update_json(&input_version, path),
-        ".plist" => update_plist(&input_version, path),
+        "xml" | "props" | "csproj" => update_xml(&input_version, path),
+        "json" => update_json(&input_version, path),
+        "plist" => update_plist(&input_version, path),
         _ if file_name == "Chart.yaml" || file_name == "Chart.yml" => update_yaml(&input_version, path),
         //_ => return Err("No file was recognized as a supported format.".into()),
 
         _ => Ok({})
     }.expect("No file was recognized as a supported format.");
-
 
     if let Ok(github_output) = std::env::var("GITHUB_OUTPUT") {
         if let Ok(mut file) = std::fs::OpenOptions::new()
@@ -48,18 +48,69 @@ fn main() -> io::Result<()> {
 }
 
 fn update_yaml(version: &String, path: &Path) -> io::Result<()> {
-    todo!()
+    // Read the YAML file
+    let contents = fs::read_to_string(path)?;
+
+    // Parse the YAML file into a serde_yaml::Value
+    let mut doc: serde_yaml::Value = serde_yaml::from_str(&contents).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+    // Update the version field
+    if let Some(map) = doc.as_mapping_mut() {
+        map.insert(serde_yaml::Value::String("version".to_string()), serde_yaml::Value::String(version.clone()));
+    }
+
+    // Write the updated YAML back to the file
+    let new_contents = serde_yaml::to_string(&doc).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    fs::write(path, new_contents)?;
+
+    Ok(())
 }
 fn update_plist(version: &String, path: &Path) -> io::Result<()> {
-    todo!()
+    // Open the plist file for reading
+    let mut file = File::open(path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    // Parse the plist
+    let mut plist_data: PlistValue = plist::from_bytes(&buffer).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+    // Update the version
+    if let PlistValue::Dictionary(ref mut dict) = plist_data {
+        dict.insert("CFBundleShortVersionString".to_string(), PlistValue::String(version.clone()));
+    }
+
+    // Write back the updated plist
+    let file = File::create(path)?;
+    plist::to_writer_xml(file, &plist_data).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+    Ok(())
 }
 fn update_json(version: &String, path: &Path) -> io::Result<()> {
-    todo!()
+    // Read the file content
+    let file_content = fs::read_to_string(path)?;
+
+    // Parse JSON
+    let mut data: JsonValue = serde_json::from_str(&file_content)?;
+
+    // Update the version
+    if let Some(obj) = data.as_object_mut() {
+        obj.insert("version".to_string(), JsonValue::String(version.clone()));
+        if let Some(packages) = obj.get_mut("packages").and_then(|p| p.as_object_mut()) {
+            if let Some(empty_key) = packages.get_mut("").and_then(|p| p.as_object_mut()) {
+                empty_key.insert("version".to_string(), JsonValue::String(version.clone()));
+            }
+        }
+    }
+
+    // Write the updated JSON back to file
+    let mut file = fs::File::create(path)?;
+    file.write_all(serde_json::to_string_pretty(&data)?.as_bytes())?;
+    file.write_all(b"\n")?; // Append a newline
+
+    Ok(())
 }
 
 fn update_xml(version: &String, path: &Path) -> io::Result<()> {
-    // let mut file = File::open(path)?;
-    // let mut contents = String::new();
     let mut file_content = fs::read_to_string(path)?;
 
     // Parse XML
@@ -72,6 +123,7 @@ fn update_xml(version: &String, path: &Path) -> io::Result<()> {
         fs::write(path, file_content)?;
     }
     // Microsoft .NET project files
+        // TODO: Identify why it reformats the entire document
     else if let Some(sdk) = root.attributes.get("Sdk") {
         if sdk.contains("Microsoft.NET.Sdk") {
             if let Some(property_group) = root.get_mut_child("PropertyGroup") {
@@ -92,6 +144,7 @@ fn update_xml(version: &String, path: &Path) -> io::Result<()> {
         }
     }
     // MSBuild Props
+        // TODO: Figure out why it reformats the whole document
     else if let Some(property_group) = root.get_mut_child("PropertyGroup") {
         for child in &mut property_group.children {
             if let XMLNode::Element(element) = child {
@@ -111,123 +164,3 @@ fn update_xml(version: &String, path: &Path) -> io::Result<()> {
     Ok(())
 }
 
-fn is_valid_semver(version: &str) -> bool {
-    match Version::parse(version) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
-}
-
-// fn update_package_json(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//     let mut json: Value = serde_json::from_str(&content)
-//         .map_err(|e| Error::new(ErrorKind::InvalidData, format!("JSON parse error: {}", e)))?;
-//
-//     if let Some(obj) = json.as_object_mut() {
-//         obj.insert("version".to_string(), json!(version));
-//     } else {
-//         return Err(Error::new(ErrorKind::InvalidData, "Invalid package.json structure"));
-//     }
-//
-//     fs::write(path, serde_json::to_string_pretty(&json)?)
-// }
-//
-// fn update_csproj(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//
-//     // Using regex for XML updating as it's simpler than full XML parsing for this task
-//     let version_regex = Regex::new(r"<(Version|AssemblyVersion|FileVersion)>(.*?)</(Version|AssemblyVersion|FileVersion)>")
-//         .map_err(|e| Error::new(ErrorKind::Other, format!("Regex error: {}", e)))?;
-//
-//     let updated_content = version_regex.replace_all(&content, |caps: &regex::Captures| {
-//         format!("<{}>{}</{}>", &caps[1], version, &caps[3])
-//     });
-//
-//     if content == updated_content {
-//         return Ok(());  // No changes were made
-//     }
-//
-//     fs::write(path, updated_content.to_string())
-// }
-//
-// fn update_plist(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//
-//     // Using regex for plist updating
-//     let bundle_version_regex = Regex::new(r"<key>CFBundleShortVersionString</key>\s*<string>(.*?)</string>")
-//         .map_err(|e| Error::new(ErrorKind::Other, format!("Regex error: {}", e)))?;
-//
-//     let updated_content = bundle_version_regex.replace_all(&content, |_: &regex::Captures| {
-//         format!("<key>CFBundleShortVersionString</key>\n\t<string>{}</string>", version)
-//     });
-//
-//     if content == updated_content {
-//         return Ok(());  // No changes were made
-//     }
-//
-//     fs::write(path, updated_content.to_string())
-// }
-//
-// fn update_manifest_json(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//     let mut json: Value = serde_json::from_str(&content)
-//         .map_err(|e| Error::new(ErrorKind::InvalidData, format!("JSON parse error: {}", e)))?;
-//
-//     if let Some(obj) = json.as_object_mut() {
-//         obj.insert("version".to_string(), json!(version));
-//     } else {
-//         return Err(Error::new(ErrorKind::InvalidData, "Invalid manifest.json structure"));
-//     }
-//
-//     fs::write(path, serde_json::to_string_pretty(&json)?)
-// }
-//
-// fn update_pubspec(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//
-//     // Using regex for YAML updating
-//     let version_regex = Regex::new(r"version:\s*(.*?)(\s|$)")
-//         .map_err(|e| Error::new(ErrorKind::Other, format!("Regex error: {}", e)))?;
-//
-//     let updated_content = version_regex.replace_all(&content, |caps: &regex::Captures| {
-//         format!("version: {}{}", version, &caps[2])
-//     });
-//
-//     if content == updated_content {
-//         return Ok(());  // No changes were made
-//     }
-//
-//     fs::write(path, updated_content.to_string())
-// }
-//
-// fn update_infoplist_strings(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//
-//     // Using regex for InfoPlist.strings updating
-//     let version_regex = Regex::new(r#""CFBundleShortVersionString"\s*=\s*"(.*?)";"#)
-//         .map_err(|e| Error::new(ErrorKind::Other, format!("Regex error: {}", e)))?;
-//
-//     let updated_content = version_regex.replace_all(&content, |_: &regex::Captures| {
-//         format!(r#""CFBundleShortVersionString" = "{}";"#, version)
-//     });
-//
-//     if content == updated_content {
-//         return Ok(());  // No changes were made
-//     }
-//
-//     fs::write(path, updated_content.to_string())
-// }
-//
-// fn update_cargo_toml(version: &str, path: &Path) -> io::Result<()> {
-//     let content = fs::read_to_string(path)?;
-//     let mut doc = content.parse::<Document>()
-//         .map_err(|e| Error::new(ErrorKind::InvalidData, format!("TOML parse error: {}", e)))?;
-//
-//     if let Some(package) = doc.as_table_mut().get_mut("package") {
-//         if let Some(table) = package.as_table_mut() {
-//             table["version"] = value(version);
-//         }
-//     }
-//
-//     fs::write(path, doc.to_string())
-// }
